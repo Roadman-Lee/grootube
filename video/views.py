c97db9ed481071a1361d72472651e75016ec8e07
Roadman <rdmshine@Roadmanui-iMac.local>
2022-06-04 19:46:55 +0900

    first commit

---
+++ video/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import redirect, render
+from django.urls import reverse
+
+# Create your views here.
+from video.models import Video
+
+
+def video_list(request):
+    video_list = Video.objects.all()
+    return render(request, "video/video_list.html", {"video_list": video_list})
+
+
+def video_new(request):
+    if request.method == "POST":
+        title = request.POST["title"]
+        video_key = request.POST["video_key"]
+        Video.objects.create(title=title, video_key=video_key)
+        return redirect(reverse("video:list"))
+
+    elif request.method == "GET":
+        return render(request, "video/video_new.html")
+
+
+def video_detail(request, video_id):
+    video = Video.objects.get(id=video_id)
+    return render(request, "video/video_detail.html", {"video": video})
